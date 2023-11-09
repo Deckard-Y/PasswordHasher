4ffeed9d89bb0740d62449f211f1629071b1f1b2
BARREL2 <barrelc12@gmail.com>
2023-11-09 23:18:43 +0900

    プロジェクト ファイルを追加します。

---
+++ PasswordHasher/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using TextCopy;
+
+public static class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        if (password == null)
+        {
+            // nameof演算子を使って引数の名前を例外に渡す
+            throw new ArgumentNullException(nameof(password));
+        }
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+
+    public static string InputChecker(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            throw new ArgumentException("Password cannot be null, empty, or whitespace.", nameof(password));
+        }
+
+        if (password.Length > 256) // 任意の長さ制限を設定する場合
+        {
+            throw new ArgumentException("Password is too long.", nameof(password));
+        }
+
+        return password;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter password to hash: ");
+        string defaultPassword = Console.ReadLine();
+        string hashedPassword = PasswordHasher.HashPassword(defaultPassword);
+        Console.WriteLine($"Hashed password: {hashedPassword}");
+        //クリップボードにコピー
+        ClipboardService.SetText(hashedPassword);
+    }
+}
---
+++ TestProject1/PasswordHasherTests.cs
@@ -0,0 +1,69 @@
+namespace TestProject1
+{
+    public class PasswordHasherTests
+    {
+        [Theory]
+        [InlineData("MyTestPassword")]
+        [InlineData("")]
+        [InlineData("ThisIsAVeryLongPasswordToEnsureThatTheHashingMechanismCanHandleLargeInputsProperly")]
+        [InlineData("特殊文字#%&")]
+        public void InputChecker_ValidatesInputsCorrectly(string password)
+        {
+            // Arrange - ここで必要な状態を設定します。
+
+            // Act - テストを行いたい操作を実行します。
+            // この場合、空の文字列や長すぎる文字列で例外が投げられることを確認することが目的です。
+            Action act = () => PasswordHasher.InputChecker(password);
+
+            // Assert - 期待される結果をアサートします。
+            // 空の文字列の場合は例外が投げられるべきです。
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                Assert.Throws<ArgumentException>(act);
+            }
+            else if (password.Length > 256)
+            {
+                Assert.Throws<ArgumentException>(act);
+            }
+            else
+            {
+                var exception = Record.Exception(act);
+                Assert.Null(exception);
+            }
+        }
+
+        [Fact]
+        public void HashPassword_DifferentPasswords_ProduceDifferentHashes()
+        {
+            // Arrange
+            var passwordOne = "password123";
+            var passwordTwo = "password124";
+
+            // Act
+            var hashOne = PasswordHasher.HashPassword(passwordOne);
+            var hashTwo = PasswordHasher.HashPassword(passwordTwo);
+
+            // Assert
+            Assert.NotEqual(hashOne, hashTwo);
+        }
+
+        [Fact]
+        public void HashPassword_WithNullInput_ThrowsArgumentNullException()
+        {
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => PasswordHasher.HashPassword(null));
+            Assert.Equal("password", exception.ParamName); // ArgumentNullExceptionのParamNameが"password"であることを確認
+        }
+
+        [Fact]
+        public void HashPassword_WithExtremelyLongInput_DoesNotThrowException()
+        {
+            // Arrange
+            var longPassword = new string('a', 10000); // 非常に長い文字列
+
+            // Act & Assert
+            var exceptionRecorded = Record.Exception(() => PasswordHasher.HashPassword(longPassword));
+            Assert.Null(exceptionRecorded); // 例外が投げられないことを確認
+        }
+    }
+}
